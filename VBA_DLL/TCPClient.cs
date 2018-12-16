1921bb762a37e43a09bee087252f26ff3d6429a9
mattwynyard <mjwynyard@gmail.com>
2018-12-17 11:08:09 +1300

    Added connect() and closeAll() methods.

--- VBA_DLL/TCPClient.cs
+++ VBA_DLL/TCPClient.cs
@@ -1,26 +1,25 @@
 ﻿using System;
 using System.Collections.Concurrent;
-using System.Collections.Generic;
-using System.Collections;
-using System.Linq;
 using System.Text;
 using System.Net.Sockets;
 using System.IO;
 using System.Threading;
 using System.Runtime.InteropServices;
+using System.Diagnostics;
 
 namespace TCPClient
 {
     [InterfaceType(ComInterfaceType.InterfaceIsDual)]
-    [Guid("01A31113-9353-44cc-A1F4-C6F1210E4B30")]
+    [Guid("01A31113-9353-44CC-A1F4-C6F1210E4B30")]
 
-    public interface _Client
+    public interface IClient
     {
         string getBuffer();
         Boolean isConnected();
         int sendCommand(String command);
+        void closeAll();
+        int connect(String path);
     }
-
 
     [ClassInterface(ClassInterfaceType.None)]
     [Guid("E2F07CD4-CE73-4102-B35D-119362624C47")]
@@ -32,25 +31,27 @@ namespace TCPClient
     /// The VBA program can call the getBuffer() method which puts the data in the queue into a string and returns it to VBA,
     /// then set the queue to null, thus acting like a buffer.
     /// </summary>
-    public class Client : _Client
+    public class Client : IClient
     {
         private ConcurrentQueue<string> dataQueue = new ConcurrentQueue<string>();
         private Boolean mLock = false;
         private Boolean mConnected = false;
         private TcpClient client;
         private NetworkStream stream;
+        private Thread readThread;
+        private Process clientProcess;
 
-     /// <summary>
-    /// The constructor for the Client which starts the Read method running on a new thread
-    /// </summary>
+        /// <summary>
+        /// The constructor for the Client which starts the Read method running on a new thread
+        /// </summary>
         public Client()
         {
-
-            Thread readThread = new Thread(Read);
+            readThread = new Thread(Read);
             readThread.Start();
-            //startTimer();
         }
-
+        /// <summary>
+        /// The constructor for the Client which starts the Read method running on a new thread
+        /// </summary>
         public void Read()
         {
             while (client == null)
@@ -58,17 +59,17 @@ namespace TCPClient
                 try
                 {
                     client = new TcpClient("localhost", 38200);
-                    //console.writeline("waiting for bluetooth...");
                     if (client.Connected == true)
                     {
                         stream = client.GetStream();
                         mConnected = true;
-
                     }
                 }
-                catch (SocketException e)
+                catch (SocketException e) //not connected to server
                 {
-                    Console.WriteLine("server not ready...");
+                    //Console.WriteLine("server not ready...");
+                    dataQueue = new ConcurrentQueue<string>();
+                    dataQueue.Enqueue(e.Message + '\n');
                     Thread.Sleep(100);
                 }
             }
@@ -84,7 +85,6 @@ namespace TCPClient
                             byte[] buffer = new byte[client.ReceiveBufferSize];
                             Int32 receiveCount = stream.Read(buffer, 0, buffer.Length);
                             received = new ASCIIEncoding().GetString(buffer, 0, receiveCount);
-                            //Console.WriteLine(received);
                             while (!mLock)
                             {
                                 mLock = true;
@@ -100,14 +100,27 @@ namespace TCPClient
                             }
                             mLock = false;
                         }
-
                     }
                     catch (IOException e)
                     {
-                        Console.WriteLine(e.Message);
+                        //Console.WriteLine(e.Message);
+                        dataQueue.Enqueue(e.Message + '\n');
                     }
                 }
             }
+        }
+
+        public int connect(String jarPath)
+        {
+            clientProcess = new Process();
+            clientProcess.StartInfo.FileName = "java";
+            clientProcess.StartInfo.Arguments = @"-jar " + jarPath;
+            clientProcess.Start();
+            if (clientProcess.HasExited || clientProcess == null)
+            {
+                return -1;
+            }
+            return clientProcess.Id;
         }
 
         //Called by VBA
@@ -119,7 +132,7 @@ namespace TCPClient
                 mLock = true;
                 if (dataQueue == null)
                 {
-                    return "null";
+                    return "";
                 }
                 String[] data = dataQueue.ToArray();
                 dataQueue = null;
@@ -135,14 +148,20 @@ namespace TCPClient
         {
             if (stream.CanWrite)
             {
-                byte[] buffer = Encoding.ASCII.GetBytes(command);
-                stream.Write(buffer, 0, buffer.Length);
-                return 0; //success
+                try
+                {
+                    byte[] buffer = Encoding.ASCII.GetBytes(command);
+                    stream.Write(buffer, 0, buffer.Length);
+                    return 0; //success
+                }
+                catch (IOException e)
+                {
+                    return -1;
+                }
             }
             else
             {
-                //failed
-                return -1;
+                return -1;//failed
             }
         }
 
@@ -150,5 +169,50 @@ namespace TCPClient
         {
             return mConnected;
         }
+
+        public void closeAll()
+        {
+            if (client != null)
+            {
+                mConnected = false;
+                stream.Close();
+                client.Close();
+            }
+            readThread = null;
+            clientProcess.Kill();
+        }
     }
 }
+
+//        static void Main(string[] args)
+//        {
+//            Client c = new Client();
+//            c.connect("C:\\IntelliJ_CameraApp.jar");
+//            int count = 0;
+//            while (true)
+//            {
+//                if (count == 200)
+//                {
+//                    Console.WriteLine("closing...");
+//                    try
+//                    {
+//                        c.closeAll();
+//                    }
+//                    catch (Exception e)
+//                    {
+//                        Console.WriteLine(e.Message);
+//                    }
+//                    break;
+//                }
+//                else
+//                {
+//                    String s = c.getBuffer();
+//                    Console.Write(s);
+//                    count++;
+//                    Thread.Sleep(1000);
+//                }
+
+//            }
+//        }
+//    }
+//}
